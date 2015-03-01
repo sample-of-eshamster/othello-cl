e98c18790e0010412c03b5c5749b7cd77975da80
eshamster <hamgoostar@gmail.com>
2015-03-01 13:24:11 +0900

    #2 change the interface of uct funcs to generalize

--- TEST/uct.lisp
+++ TEST/uct.lisp
@@ -7,10 +7,11 @@
   (prove:ok (uct-node-p (car (make-a-uct-node (make-a-move 3 4))))))
 
 (defun make-expanded-node (game visit-times intv)
-  (let ((tree (make-a-uct-node nil)))
+  (let ((tree (make-a-uct-node nil))
+	(param (make-uct-param :expand-intv intv)))
     (dotimes (x visit-times)
       (setf (uct-node-num (get-node-value tree)) (+ x 1))
-      (setf tree (expand-child-if-needed game tree intv)))
+      (setf tree (expand-child-if-needed game tree param)))
     tree))
   
 (prove:subtest "Test expand-child-if-needed"
@@ -55,17 +56,29 @@
 		   (incf count 1))
 		 ;; test if the node that have not been visited is selected
 		 (setf (uct-node-num (car (get-nth-child target tree))) 0)
-		 (prove:is (select-uct-child tree (game-turn game) ucb-coef)
+		 (prove:is (select-uct-child game tree *def-uct-param*)
 			   (get-nth-child target tree)
 			   :test #'equalp)
       
 		 ;; test if the max uct node is selected
 		 (setf (uct-node-num (car (get-nth-child target tree))) (+ target 1))
 		 (test-multf (uct-node-sum (car (get-nth-child target tree))) -1)
-		 (prove:is (select-uct-child tree (game-turn game) ucb-coef)
+		 (prove:is (select-uct-child game tree *def-uct-param*)
 			   (get-nth-child target tree)
 			   :test #'equalp))))
       (test-select-uct-child 4 *white*)
       (test-select-uct-child 5 *black*))))
+
+(prove:subtest "Test reflect-sim-result"
+  (let ((tree (make-a-uct-node nil))
+	(game (init-game))
+	(uct-param (make-def-uct-param)))
+    (prove:is (reflect-sim-result game tree uct-param -1) -1)
+    (prove:is (uct-node-num (get-node-value tree)) 1)
+    (prove:is (uct-node-sum (get-node-value tree)) -1)
+    
+    (prove:is (reflect-sim-result game tree uct-param 9) 9)
+    (prove:is (uct-node-num (get-node-value tree)) 2)
+    (prove:is (uct-node-sum (get-node-value tree)) 8)))
 
 (prove:finalize)
--- uct.lisp
+++ uct.lisp
@@ -13,15 +13,23 @@
 
 ; all evaluated values are from white player's perspective
 
-(defparameter *def-uct-param* (make-hash-table))
-(setf (gethash 'intv-to-expand *def-uct-param*) 2)
-(setf (gethash 'ucb-coef *def-uct-param*) 1)
-(setf (gethash 'fn-make-policy *def-uct-param*) #'make-uniform-policy)
+(defstruct uct-param
+  expand-intv
+  ucb-coef
+  fn-make-policy)
+
+(defun make-def-uct-param ()
+  (make-uct-param :expand-intv 2
+		  :ucb-coef 1
+		  :fn-make-policy #'make-uniform-policy))
+  
+(defparameter *def-uct-param* (make-def-uct-param))
 
 ; This function is destructive about "unexpanded-moves"
-(defun expand-child-if-needed (game tree expand-intv)
+(defun expand-child-if-needed (game tree uct-param)
   (let* ((node (get-node-value tree))
-	 (moves (uct-node-unexpanded-moves node)))
+	 (moves (uct-node-unexpanded-moves node))
+	 (expand-intv (uct-param-expand-intv uct-param)))
     (if (eq moves 'has-not-made-child)
 	(progn (setf moves (make-moves game))
 	       (setf (uct-node-unexpanded-moves node) moves)))
@@ -33,13 +41,20 @@
       (setf (uct-node-unexpanded-moves node) (cdr moves))
       (add-child tree (make-a-uct-node move)))))
     
-(defun select-uct-child (parent now-turn ucb-coef)
+(defun select-uct-child (game parent uct-param)
   (if (not (has-children parent))
       (return-from select-uct-child nil))
-  (select-max-child (lambda (node)
-		      (calc-ucb (uct-node-sum node)
-				(uct-node-num node)
-				(uct-node-num (get-node-value parent))
-				:coef  ucb-coef
-				:turn now-turn))
-		    parent))
+  (let ((ucb-coef (uct-param-ucb-coef uct-param))
+	(now-turn (game-turn game)))
+    (select-max-child (lambda (node)
+			(calc-ucb (uct-node-sum node)
+				  (uct-node-num node)
+				  (uct-node-num (get-node-value parent))
+				  :coef  ucb-coef
+				  :turn now-turn))
+		      parent)))
+
+(defun reflect-sim-result (game uct-tree uct-param result)
+  (incf (uct-node-sum (get-node-value uct-tree)) result)
+  (incf (uct-node-num (get-node-value uct-tree)))
+  result)
