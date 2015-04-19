cca7b4adc28633c4d59c08b2cdce489e136ce63b
eshamster <hamgoostar@gmail.com>
2015-04-19 19:44:38 +0900

    add some tree utils

--- TEST/tree.lisp
+++ TEST/tree.lisp
@@ -1,13 +1,21 @@
-(prove:plan 4)
+(prove:plan 6)
 
 (prove:subtest "Test add-child"
   (prove:is (add-child '(1) 2) '(1 (2)))
+  (prove:is (add-child '(1) 2 3) '(1 (2) (3)))
   (prove:is (add-child '(1) '(2)) '(1 (2)))
   (prove:is (add-child '(1 (2)) '3) '(1 (2) (3)))
   (prove:is (add-child '(1) (add-child '(2) 3)) '(1 (2 (3))))
   (prove:is (add-child '(1) nil) '(1 (nil)))
   (prove:is (add-child '(1) `(,nil)) '(1 (nil))))
 
+(prove:subtest
+ "Test do-children"
+ (defparameter *test-tree* (add-child '(1) 2 3 4))
+ (defparameter *test-list* nil)
+ (do-children (node *test-tree*)
+   (setf *test-list* (cons (+ (get-node-value node) 1) *test-list*)))
+ (prove:is *test-list* '(5 4 3)))
 
 (prove:subtest "Test get-num-child"
   (defparameter *test-tree* '(1))
@@ -22,19 +30,27 @@
   (prove:ok (has-children *test-tree*))
   (prove:is (get-num-children *test-tree*) 2))
 
+(prove:subtest "Test tree size and depth"
+  (defparameter *test-tree*
+    (add-child '(1) (add-child '(2) 3 4 5) 6))
+  (prove:is (get-tree-size *test-tree*) 6)
+  (prove:is (get-tree-depth *test-tree*) 2))
 
 (prove:subtest "Test some getter funcs"
+  (defparameter *test-tree* (add-child '(1) 2 3 4))
   (prove:is (get-node-value *test-tree*) 1)
   
-  (prove:is (get-children *test-tree*) '((2) (3)))
+  (prove:is (get-children *test-tree*) '((2) (3) (4)))
   (prove:is (get-children '(1)) nil)
+
+  (prove:is (get-rest-children (get-children *test-tree*)) '((3) (4)))
 
   (prove:is (get-nth-child 0 *test-tree*) '(2))
   (prove:is (get-nth-child 1 *test-tree*) '(3))
-  (prove:is (get-nth-child 2 *test-tree*) nil)
+  (prove:is (get-nth-child 2 *test-tree*) '(4))
+  (prove:is (get-nth-child 3 *test-tree*) nil)
   (setf (car (get-nth-child 1 *test-tree*)) 5)
-  (prove:is (get-nth-child 1 *test-tree*) '(5))
-  )
+  (prove:is (get-nth-child 1 *test-tree*) '(5)))
 
 (prove:subtest "Test select max funcs"
   (prove:is (select-max-child #'(lambda(val) (* val -1)) *test-tree*) '(2))
--- tree.lisp
+++ tree.lisp
@@ -1,22 +1,48 @@
 (defmacro get-node-value (node)
   `(car ,node))
 
-(defun get-children (node)
-  (cdr node))
+(defmacro get-children (node)
+  `(cdr ,node))
+
+(defmacro get-rest-children (children)
+  `(cdr ,children))
 
 (defmacro get-nth-child (n node)
   `(nth ,n (get-children ,node)))
 
+(defmacro do-children (name-tree &body body)
+  `(dolist (,(car name-tree) (get-children ,(cadr name-tree))) ,@body))
+
 (defun has-children (node)
   (not (null (get-children node))))
 
-(defun add-child (parent child)
-  (if (and (not (null child)) (listp child) (listp (cdr child)))
-      (append parent `(,child))
-      (append parent `((,child)))))
+(defun add-child (parent &rest children)
+  (append parent
+	  (mapcar (lambda (child)
+		    (if (and (not (null child)) (listp child) (listp (cdr child)))
+			`,child
+			`(,child))) children)))
 
 (defun get-num-children (node)
   (- (length node) 1))
+
+(defun get-tree-size (tree)
+  (labels ((f (node)
+ 	     (if (null node) (return-from f 0))
+ 	     (let ((sum 1))
+	       (do-children (child node)
+		 (incf sum (f child)))
+	       sum)))
+  (f tree)))
+
+(defun get-tree-depth (tree)
+  (labels ((f (node)
+	     (if (null node) (return-from f 0))
+	     (let ((depth 0))
+	       (do-children (child node)
+		 (setf depth (max depth (+ (f child) 1))))
+	       depth)))
+    (f tree)))
 
 (defun select-max-child (fn-calc-value parent)
   (if (not (has-children parent))
