1357251f761a104e968478220e3dae26f2c404b7
eshamster <hamgoostar@gmail.com>
2015-05-06 21:44:20 +0900

    #1 refactor minimax and make a test of it

--- TEST/minimax.lisp
+++ TEST/minimax.lisp
@@ -1,38 +1,55 @@
-(prove:plan 0)
+(prove:plan 2)
+
+(load "TEST/test-util.lisp")
+
+(prove:subtest
+    "Test eval-game-by-minimax"
+  (prove:subtest
+      "Check selection"
+    (labels ((get-score (node)
+	       (minimax-node-score (get-node-value node)))
+	     (prove-selection (game node)
+	       (prove:is (get-score node)
+			 (get-score
+			  (select-max-child
+			   #'(lambda (child)
+			       (* (minimax-node-score child)
+				  (if (= (game-turn game) *white*) 1 -1)))
+			   node))))   
+	     (test-at (start-depth)
+	       (let* ((depth 4)
+		      (game (make-nth-test-game start-depth))
+		      (first-game-depth (get-game-depth game))
+		      (eval-tree (eval-game-by-minimax game depth t)))
+		 (prove-selection game eval-tree)
+		 (prove:is (get-tree-depth eval-tree) depth)
+		 (prove:is (get-game-depth game) first-game-depth)
+		 (do-children (child eval-tree)
+		   (do-in-move-reverse
+		       game
+		       (minimax-node-move (get-node-value child))
+		     (prove-selection game child))))))
+      (test-at 4)
+      (test-at 5)))
+
+  (prove:subtest
+      "Check near the game end"
+    (let* ((depth 6)
+	   (game (make-nth-test-game 58))
+	   (eval-tree (eval-game-by-minimax game depth t)))
+      (assert (not (is-game-end game)))
+      (prove:ok (< (get-tree-depth eval-tree) depth)))))
+
+(prove:subtest
+    "Test select-move-by-minimax"
+  (labels ((test-func (fn)
+	     (let ((depth 4))
+	       (dolist (start-depth '(4 5))
+		 (let* ((game (make-nth-test-game start-depth))
+			(move (select-move-by-minimax game depth fn)))
+		   (prove:ok (check-move-valid (game-board game)
+					       (car move) (cdr move)
+					       (game-turn game))))))))
+    (test-func #'eval-game-by-minimax)))
 
 (prove:finalize)
-
-;; (defstruct minimax-node
-;;   move
-;;   score)
-
-;; (defun eval-game-by-minimax (game depth &optional (is-all-tree nil))
-;;   (labels ((make-a-node (value move)
-;; 	     (list (list value move)))
-;; 	   (eval-recurse (move-from rest-depth)
-;; 	     (if (or (>= 0 rest-depth) (is-game-end game))
-;; 		 (return-from eval-recurse (make-a-node
-;; 					    (eval-game-static game *white*) move-from)))
-;; 	     (let ((node (make-a-node nil move-from))
-;; 		   (turn (game-turn game)))
-;; 	       (dolist (move (make-moves game))
-;; 		 (move-game game (car move) (cdr move))
-;; 		 (let* ((child (eval-recurse move (- rest-depth 1)))
-;; 			(value (caar child)))
-;; 		   (cond ((null (caar node)) (setf (caar node) value))
-;; 			 ((and (eq turn *white*) (< (caar node) value))
-;; 			  (setf (caar node) value))
-;; 			 ((and (eq turn *black*) (> (caar node) value))
-;; 			  (setf (caar node) value)))
-;; 		   (if (or is-all-tree (eq depth rest-depth))
-;; 		       (setf node (add-child node child))))
-;; 		 (reverse-game game))
-;; 	       node)))
-;;     (eval-recurse (make-a-move -1 -1) depth)))
-		 
-;; (defun select-move-by-minimax (game depth fn-eval)
-;;   (cadar (select-max-child #'(lambda (node)
-;; 			       (* (car node)
-;; 				  (if (eq (game-turn game) *white*) 1 -1)))
-;; 			   (funcall fn-eval game depth))))
-
--- minimax.lisp
+++ minimax.lisp
@@ -2,33 +2,42 @@
   move
   score)
 
+; Evaluation values that Nodes in the created tree have
+; are calculated from the White point of view
 (defun eval-game-by-minimax (game depth &optional (is-all-tree nil))
-  (labels ((make-a-node (value move)
-	     (list (list value move)))
-	   (eval-recurse (move-from rest-depth)
+  (labels ((make-a-node (move)
+	     (list (make-minimax-node :move move :score nil)))
+	   (set-score (node score)
+	     (if (null node)
+		 score
+		 (setf (minimax-node-score (get-node-value node)) score)))
+	   (eval-recurse (parent rest-depth)
 	     (if (or (>= 0 rest-depth) (is-game-end game))
-		 (return-from eval-recurse (make-a-node
-					    (eval-game-static game *white*) move-from)))
-	     (let ((node (make-a-node nil move-from))
-		   (turn (game-turn game)))
+		 (return-from eval-recurse
+		   (set-score parent (eval-game-static game *white*))))
+	     (let ((best-score nil)
+		   (turn (game-turn game))
+		   (saves-child (or is-all-tree (eq depth rest-depth))))
 	       (dolist (move (make-moves game))
-		 (move-game game (car move) (cdr move))
-		 (let* ((child (eval-recurse move (- rest-depth 1)))
-			(value (caar child)))
-		   (cond ((null (caar node)) (setf (caar node) value))
-			 ((and (eq turn *white*) (< (caar node) value))
-			  (setf (caar node) value))
-			 ((and (eq turn *black*) (> (caar node) value))
-			  (setf (caar node) value)))
-		   (if (or is-all-tree (eq depth rest-depth))
-		       (setf node (add-child node child))))
-		 (reverse-game game))
-	       node)))
-    (eval-recurse (make-a-move -1 -1) depth)))
+		 (do-in-move-reverse game move
+		   (let* ((child (if saves-child (make-a-node move)))
+			  (child-score (eval-recurse child (- rest-depth 1))))
+		     (when (or (null best-score)
+			       (and (eq turn *white*) (< best-score child-score))
+			       (and (eq turn *black*) (> best-score child-score)))
+		       (setf best-score child-score))
+		     (when saves-child
+		       (assert (not (null child)))
+		       (insert-child parent child)))))
+		 (set-score parent best-score))))
+    (let ((node (make-a-node (make-a-move -1 -1))))
+      (eval-recurse node depth)
+      node)))
 		 
 (defun select-move-by-minimax (game depth fn-eval)
-  (cadar (select-max-child #'(lambda (node)
-			       (* (car node)
-				  (if (eq (game-turn game) *white*) 1 -1)))
-			   (funcall fn-eval game depth))))
-
+  (minimax-node-move (get-node-value
+		      (select-max-child
+		       #'(lambda (node)
+			   (* (minimax-node-score node)
+			      (if (eq (game-turn game) *white*) 1 -1)))
+		       (funcall fn-eval game depth)))))
