83242b71f59c158fcc965cc3803b4722922abac5
eshamster <hamgoostar@gmail.com>
2015-06-14 19:07:01 +0900

    fix TEST/all.lisp to correctly check success of failure of each test

--- TEST/all.lisp
+++ TEST/all.lisp
@@ -1,6 +1,6 @@
 (ql:quickload :prove)
 
-(defparameter *reporter* :dot)
+(defparameter *reporter* :tap)
 
 (defparameter *test-list* (list "define"
 				"utils"
@@ -17,19 +17,32 @@
 				"player"
 				"game-master"))
 
+(defun check-prove-result (str)
+  (let ((result-line (car (last (ppcre:split "\\n" str)))))
+    (if (ppcre:all-matches "passed" result-line)
+	t
+	(progn (format t "~A~%" str)
+	       nil))))
+
 (defparameter *result-list* nil)
+(defparameter *str-out* (make-string-output-stream))
+(setf prove:*test-result-output* *str-out*)
 
 (dolist (target *test-list*)
   (format t "#### Test ~A ####~%" target)
+  (prove:run
+   (pathname (format nil "TEST/~A.lisp" target))
+   :reporter *reporter*)
   (setf *result-list*
 	(cons
 	 (list target
-	       (prove:run
-		(pathname (format nil "TEST/~A.lisp" target))
-		:reporter *reporter*))
+	       (check-prove-result (get-output-stream-string *str-out*)))
 	 *result-list*)))
 
+(setf prove:*test-result-output* t)
+
 (setf *result-list* (reverse *result-list*))
+(fresh-line)
 (dolist (result *result-list*)
   (format t "~15@A ... ~A~%"
 	  (car result)
