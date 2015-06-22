a2b79f32391b61f598d2f181a9fba3d1bdfc33da
eshamster <hamgoostar@gmail.com>
2015-06-23 00:13:13 +0900

    #13 #14 make prob-store for random-move

--- random-move.lisp
+++ random-move.lisp
@@ -50,3 +50,32 @@
 (defun move-by-random-policy (game fn-make-policy &key (prob-store (make-prob-store)))
   (let ((move (decide-move-by-random-policy game fn-make-policy (random 1.0) prob-store)))
     (move-game game (move-x move) (move-y move))))
+
+; --------------------- ;
+
+(defstruct prob-store
+  (count 0)
+  (probs (make-array *max-move-store* :element-type 'number)))
+
+(defun reset-prob-store (store)
+  (setf (prob-store-count store) 0)
+  store)
+
+(defun add-to-prob-store (store prob)
+  (setf (aref (prob-store-probs store) (prob-store-count store)) prob)
+  (incf (prob-store-count store))
+  store)
+
+(defmacro do-prob-store (name<>store &body body)
+  (let ((i (gensym))
+	(g-store (gensym)))
+    `(let ((,g-store ,(cadr name<>store)))
+       (dotimes (,i (prob-store-count ,g-store))
+	 (let ((,(car name<>store) (aref (prob-store-probs ,g-store) ,i)))
+	   ,@body)))))
+
+(defmacro get-nth-prob (n store)
+  `(cond ((null ,n) nil)
+	 ((< ,n 0)  nil)
+	 ((>= ,n (prob-store-count ,store)) nil)
+	 (t (aref (prob-store-probs ,store) ,n))))
